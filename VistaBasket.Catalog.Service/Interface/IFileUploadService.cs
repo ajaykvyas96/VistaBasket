f21ab95faf7f781a0879f83b4256e9b07f49e223
ajjubhai <ajaykvyas96@gmail.com>
2024-02-29 09:15:56 +0530

    filw upload service

---
+++ VistaBasket.Catalog.Service/Interface/IFileUploadService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VistaBasket.Catalog.Entities.Entities.Enum;
+
+namespace VistaBasket.Catalog.Service.Interface
+{
+    public interface IFileUploadService
+    {
+        Task<string> UploadFile(FilePathEnum fileLocation, IFormFile file, List<string> supportedTypes = null);
+        Task<bool> DeleteFile(FilePathEnum fileLocation, string filePath);
+        Task<string> GetFileUrl(FilePathEnum fileLocation, string fileName);
+    }
+}
